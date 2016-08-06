f92a88291cb41ad7f4684da624b3aa48f1ef0046
Steven Halase <steven.halase@gmail.com>
2016-08-05 23:39:00 -0600

    Developed Raid Progress functionality

--- www/home/home-controller.js
+++ www/home/home-controller.js
@@ -21,4 +21,89 @@ angular.module('guildApp')
       img : './images/news/news-item3.jpg',
       story : 'Sit ei ferri vivendo insolens, ei docendi principes has. Sea dicat nonumy iudicabit te, at quo albucius abhorreant. Cu mel magna platonem. Eum natum phaedrum menandri ad, vidit nostro adipisci ne pri. Enim nostrud in eos, pro apeirian intellegam an.'
     }]
+
+    hCtrl.raids = [{
+      id : '1',
+      title : 'The Emerald Nightmare',
+      difficulties : ['Normal', 'Heroic', 'Mythic'],
+      bosses : [{
+        name: 'Nythendra',
+        alive: false
+      },{
+        name: 'Il’gynoth',
+        alive: false
+      },{
+        name: 'Heart of corruption',
+        alive: false
+      },{
+        name: 'Elerethe Renefal',
+        alive: false
+      },{
+        name: 'Ursoc',
+        alive: false
+      },{
+        name: 'Dragons of Nightmare',
+        alive: true
+      },{
+        name: 'Cenarius',
+        alive: true
+      },{
+        name: 'Xavius',
+        alive: true
+      }],
+      img: '../images/emerald-nightmare.jpg'
+    },{
+      id : '2',
+      title : 'The Nighthold',
+      difficulties : ['Normal', 'Heroic', 'Mythic'],
+      bosses : [{
+        name: 'Skorpyron',
+        alive: false
+      },{
+        name: 'Chronomatic Anomaly',
+        alive: false
+      },{
+        name: 'Trilliax',
+        alive: false
+      },{
+        name: 'Spellblade Aluriel',
+        alive: true
+      },{
+        name: 'Tichondrius',
+        alive: true
+      },{
+        name: 'Krosus',
+        alive: true
+      },{
+        name: 'High Botanist Tel’arn',
+        alive: true
+      },{
+        name: 'Star Augur Etraeus',
+        alive: true
+      },{
+        name: 'Grand Magistrix Elisandre',
+        alive: true
+      },{
+        name: 'Gul’dan',
+        alive: true
+      }],
+      img: '../images/nighthold.jpg'
+    }]
+    for (let i = 0; i < hCtrl.raids.length; i++) {
+      let bossesDefeated = 0;
+      for (let j = 0; j < hCtrl.raids[i].bosses.length; j++) {
+        if (hCtrl.raids[i].bosses[j].alive === false) {
+          bossesDefeated = bossesDefeated + 1;
+          console.log(bossesDefeated)
+        }
+      }
+      console.log('Bosses alive: ', bossesDefeated)
+      hCtrl.raids[i].progress = ((bossesDefeated / hCtrl.raids[i].bosses.length) * 100);
+    }
+
+    hCtrl.openRaidDetails = function(id) {
+      let modalID = '#raidModal' + id;
+      console.log('opening', modalID)
+      $(modalID).modal('show')
+    }
   }
